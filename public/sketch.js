e1f44a0f5e16915311bf9b05aeb545f0af2ba06a
C-Groz <charliegroz84@gmail.com>
2022-04-10 14:52:19 -0400

    rect

--- public/sketch.js
+++ public/sketch.js
@@ -25,6 +25,7 @@ function draw() {
     fill(105,105,105);
     text("Players in lobby: " + players.length, windowWidth/2, windowHeight/2 - 250);
 
+    rect(windowWidth/2 - 50, windowHeight/2 - 200, 100, 50);
 
 
   }else{
