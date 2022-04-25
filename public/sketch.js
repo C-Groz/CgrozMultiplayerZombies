bf2cc6c062bf6af21f676861a42a5befa085851a
C-Groz <charliegroz84@gmail.com>
2022-04-25 09:17:04 -0400

    .

--- public/sketch.js
+++ public/sketch.js
@@ -1,5 +1,5 @@
 
-const socket = io.connect('https://safe-sands-40981.herokuapp.com/');
+const socket = io.connect('https://safe-sands-40981.herokuapp.com/', { transports : ['websocket'] });
 //const socket = io.connect('localhost:3000');
 
 let gameActive = false;
